031f6a0eec271ec564b00306cb01d9cb6bccab68
Robert P <porobertdev@gmail.com>
2023-07-21 23:51:22 +0300

    js: define game's settings

---
+++ script.js
@@ -0,0 +1,8 @@
+// Game Settings
+// TO DO: better variable naming for weapons?
+// weapons
+const weaponOne = 'rock';
+const weaponTwo = 'paper';
+const weaponThree = 'scissors';
+// rounds
+const GAME_ROUNDS = 5;
